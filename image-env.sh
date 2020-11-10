afcb2e4997932d09c05dce0babc151cde63a6670
cvicens <carlos.vicens.alonso@gmail.com>
2020-11-10 19:50:24 +0100

    update to CHE 7.21.1, oc 4.5, kn and tkn also updated

--- image-env.sh
+++ image-env.sh
@@ -3,5 +3,5 @@
 export REGISTRY=quay.io
 export REGISTRY_USER_ID=cvicensa
 export IMAGE_NAME=cnw-che-stack
-export IMAGE_VERSION=7.3.1
+export IMAGE_VERSION=7.21.1
 export FROM_IMAGE=quay.io/eclipse/che-java11-maven:${IMAGE_VERSION}
