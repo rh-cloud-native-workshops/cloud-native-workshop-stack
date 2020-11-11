c6560e6f2030bb979e06d81404c48ed0bc901191
cvicens <carlos.vicens.alonso@gmail.com>
2020-11-11 09:50:51 +0100

    Version 7.19.2

--- image-build.sh
+++ image-build.sh
@@ -3,3 +3,8 @@
 . ./image-env.sh
 
 docker build -t $IMAGE_NAME:$IMAGE_VERSION --build-arg FROM_IMAGE="${FROM_IMAGE}"  .
+
+docker build -t $IMAGE_NAME:$IMAGE_VERSION --build-arg FROM_IMAGE="${FROM_IMAGE}"  . && \
+    git tag -a $IMAGE_VERSION -m "Version $IMAGE_VERSION" && \
+    git commit -a -m "Version $IMAGE_VERSION" && \
+    git push origin $IMAGE_VERSION
