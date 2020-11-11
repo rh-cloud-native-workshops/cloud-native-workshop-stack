ef5b8c106307bfda0abd5d0e06f4a5191d77e66b
cvicens <carlos.vicens.alonso@gmail.com>
2020-11-11 10:06:50 +0100

    Version 7.21.0

--- image-push.sh
+++ image-push.sh
@@ -4,7 +4,3 @@
 
 docker tag $IMAGE_NAME:$IMAGE_VERSION $REGISTRY/$REGISTRY_USER_ID/$IMAGE_NAME:$IMAGE_VERSION
 docker push $REGISTRY/$REGISTRY_USER_ID/$IMAGE_NAME:$IMAGE_VERSION
-
-git tag -a $IMAGE_VERSION -m "Version $IMAGE_VERSION" && \
-git commit -a -m "Version $IMAGE_VERSION" && \
-git push origin $IMAGE_VERSION
